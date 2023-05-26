e24cba95487323bf46463ff18c3f160762a3e6af
camille <lc.laignel@gmail.com>
2023-05-26 14:52:40 +0200

    feat: Add error handling for tours

--- src/controllers/tour.controller.ts
+++ src/controllers/tour.controller.ts
@@ -1,90 +1,99 @@
 import { NextFunction, Request, Response } from "express";
 import Tour from "../models/tour.model";
+import catchAsync from "../shared/utils/catchAsync.util";
+import AppError from "../shared/utils/AppError.util";
+import { EMPTY_RESULT } from "../shared/messages/error.message";
 
-export const getAllTours = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const tours = await Tour.find();
+export const getAllTours = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const tours = await Tour.find();
 
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
+    if (!tours) {
+      return next(new AppError(EMPTY_RESULT, 404));
+    }
 
-export const createTour = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const tour = await Tour.create({
-    title: req.body.title,
-    summary: req.body.summary,
-    description: req.body.description,
-    price: req.body.price,
-    duration: req.body.duration,
-    maxGroupSize: req.body.maxGroupSize,
-  });
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  }
+);
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-};
+export const createTour = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const tour = await Tour.create({
+      title: req.body.title,
+      summary: req.body.summary,
+      description: req.body.description,
+      price: req.body.price,
+      duration: req.body.duration,
+      maxGroupSize: req.body.maxGroupSize,
+    });
 
-export const getTour = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const id = req.params.id;
-  const tour = await Tour.findById(id);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  }
+);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-};
+export const getTour = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const tour = await Tour.findById(id);
 
-export const updateTour = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const id = req.params.id;
+    if (!tour) {
+      return next(new AppError(EMPTY_RESULT, 404));
+    }
 
-  const tour = await Tour.findByIdAndUpdate(id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  }
+);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-};
+export const updateTour = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
 
-export const deleteTour = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const id = req.params.id;
-  const tour = await Tour.findByIdAndDelete(id);
+    const tour = await Tour.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(200).json({
-    status: "success",
-    message: `Le document ayant l'identifiant ${id} a été supprimé avec succès.`,
-  });
-};
+    if (!tour) {
+      return next(new AppError(EMPTY_RESULT, 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  }
+);
+
+export const deleteTour = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const tour = await Tour.findByIdAndDelete(id);
+
+    if (!tour) {
+      return next(new AppError(EMPTY_RESULT, 404));
+    }
+    
+    res.status(200).json({
+      status: "success",
+      message: `Le document ayant l'identifiant ${id} a été supprimé avec succès.`,
+    });
+  }
+);
